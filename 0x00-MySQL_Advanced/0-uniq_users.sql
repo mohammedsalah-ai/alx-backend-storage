a6022678dc2d69a50782dcc19569d3b146420cd6
Mohammed Salah <MohammedSalah.SuperM159@gmail.com>
2024-04-25 23:10:58 +0200

    TASK ZERO

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- TASK ZERO
+-- script that creates a table users
+CREATE TABLE IF NOT EXISTS users (
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+);
